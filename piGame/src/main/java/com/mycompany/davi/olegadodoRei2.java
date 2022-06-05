299386bc6eed8cda5809d66870acea68664f5b43
davi <davi_@DESKTOP-FCGGOL0>
2022-06-05 16:29:42 -0300

    Projetopi

--- piGame/src/main/java/com/mycompany/davi/olegadodoRei2.java
+++ piGame/src/main/java/com/mycompany/davi/olegadodoRei2.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+package com.mycompany.davi;
+
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
-/**
- *
- * @author davi_
- */
+
+
 public class olegadodoRei2 {
 
     static int ataqueJogador() {
